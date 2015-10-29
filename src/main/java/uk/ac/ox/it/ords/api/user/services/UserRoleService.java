4eae6621b89ce00754044a777df4bec4189b01fe
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-29 22:58:21 +0000

    Implemented UserRole service

--- src/main/java/uk/ac/ox/it/ords/api/user/services/UserRoleService.java
+++ src/main/java/uk/ac/ox/it/ords/api/user/services/UserRoleService.java
@@ -1,15 +1,47 @@
 package uk.ac.ox.it.ords.api.user.services;
 
 import java.util.List;
+import java.util.ServiceLoader;
 
 import uk.ac.ox.it.ords.api.user.model.User;
+import uk.ac.ox.it.ords.api.user.services.impl.hibernate.UserRoleServiceImpl;
 import uk.ac.ox.it.ords.security.model.UserRole;
 
 public interface UserRoleService {
 	
-	public List<UserRole> getUserRolesForUser(User user);
+	public List<UserRole> getUserRolesForUser(User user) throws Exception;
 	public void createUserRole(UserRole userRole) throws Exception;
 	public void updateUserRole(UserRole userRole) throws Exception;
 	public void deleteUserRole(UserRole userRole) throws Exception;
+	
+	/**
+	 * Factory for obtaining implementations
+	 */
+    public static class Factory {
+		private static UserRoleService provider;
+	    public static UserRoleService getInstance() {
+	    	//
+	    	// Use the service loader to load an implementation if one is available
+	    	// Place a file called uk.ac.ox.oucs.ords.utilities.csv in src/main/resources/META-INF/services
+	    	// containing the classname to load as the CsvService implementation. 
+	    	// By default we load the Hibernate implementation.
+	    	//
+	    	if (provider == null){
+	    		ServiceLoader<UserRoleService> ldr = ServiceLoader.load(UserRoleService.class);
+	    		for (UserRoleService service : ldr) {
+	    			// We are only expecting one
+	    			provider = service;
+	    		}
+	    	}
+	    	//
+	    	// If no service provider is found, use the default
+	    	//
+	    	if (provider == null){
+	    		provider = new UserRoleServiceImpl();
+	    	}
+	    	
+	    	return provider;
+	    }
+	}
 
 }
---
+++ src/main/java/uk/ac/ox/it/ords/api/user/services/impl/hibernate/UserRoleServiceImpl.java
@@ -0,0 +1,85 @@
+package uk.ac.ox.it.ords.api.user.services.impl.hibernate;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.ox.it.ords.api.user.model.User;
+import uk.ac.ox.it.ords.api.user.services.UserRoleService;
+import uk.ac.ox.it.ords.security.model.UserRole;
+
+public class UserRoleServiceImpl implements UserRoleService {
+	
+	
+	private static Logger log = LoggerFactory.getLogger(UserRoleServiceImpl.class);
+
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public UserRoleServiceImpl() {
+		setSessionFactory (HibernateUtils.getSessionFactory());
+	}
+
+	public List<UserRole> getUserRolesForUser(User user) throws Exception {
+		Session session = this.sessionFactory.getCurrentSession();
+		try {
+			session.beginTransaction();
+			@SuppressWarnings("unchecked")
+			List<UserRole> userRoles = session.createCriteria(UserRole.class)
+					.add(Restrictions.eq("principalName", user.getPrincipalName()))
+					.list();
+			session.getTransaction().commit();
+			return userRoles;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			throw e;
+		} finally {
+			  HibernateUtils.closeSession();
+		}
+	}
+
+	public void createUserRole(UserRole userRole) throws Exception {
+		Session session = this.sessionFactory.getCurrentSession();
+		try {
+			session.beginTransaction();
+			session.save(userRole);
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			log.error("Error creating user role", e);
+			session.getTransaction().rollback();
+			throw new Exception("Cannot create user role",e);
+		} finally {
+			  HibernateUtils.closeSession();
+		}
+	}
+
+	public void updateUserRole(UserRole userRole) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteUserRole(UserRole userRole) throws Exception {
+		Session session = this.sessionFactory.getCurrentSession();
+		try {
+			session.beginTransaction();
+			session.delete(userRole);
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			log.error("Error deleting user role", e);
+			session.getTransaction().rollback();
+			throw new Exception("Cannot delete user role",e);
+		} finally {
+			  HibernateUtils.closeSession();
+		}
+
+	}
+
+}
