ecec9dd37d8e88d436ddc2f6c40722319b5cc97e
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-02 16:48:05 +0000

    Removed unused imports

--- src/main/java/uk/ac/ox/it/ords/api/user/resources/UserResource.java
+++ src/main/java/uk/ac/ox/it/ords/api/user/resources/UserResource.java
@@ -1,12 +1,8 @@
 package uk.ac.ox.it.ords.api.user.resources;
 
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
