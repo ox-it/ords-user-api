fa93820122d746e3e25e30c8a22e600c2c4d55a8
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2016-01-18 14:47:07 +0000

    Added project name for contact requests

--- src/main/java/uk/ac/ox/it/ords/api/user/model/ContactRequest.java
+++ src/main/java/uk/ac/ox/it/ords/api/user/model/ContactRequest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ox.it.ords.api.user.model;
 
 /**
@@ -10,9 +25,10 @@ public class ContactRequest {
 	
 	// The ORDS user to contact
 	int userId;
-	
 	// The name of the person making contact
 	String name;
+	// The project this is related to
+	String project;
 	// Their message
 	String message;
 	// Their return email address
@@ -41,6 +57,12 @@ public class ContactRequest {
 	}
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
 	}
 
 }
